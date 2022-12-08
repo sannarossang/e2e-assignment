80b4d38ddd7f88846fc9eaa67cc810322efd9b69
sannarossang <sanna.rossang@gmail.com>
2022-12-08 14:07:36 +0100

    adding test

--- cypress/e2e/__mocks__/movieservice.ts
+++ cypress/e2e/__mocks__/movieservice.ts
@@ -6,28 +6,32 @@ export const mockData: IOmdbResponse = {
       Title: "The Lion King",
       imdbID: "tt0110357",
       Type: "movie",
-      Poster: "https://imageofthelionking.jpg",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BYTYxNGMyZTYtMjE3MS00MzNjLWFjNmYtMDk3N2FmM2JiM2M1XkEyXkFqcGdeQXVyNjY5NDU4NzI@._V1_SX300.jpg",
       Year: "1994",
     },
     {
       Title: "The Lion King",
       imdbID: "tt0110357",
       Type: "movie",
-      Poster: "https://imageofthelionking.jpg",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BYTYxNGMyZTYtMjE3MS00MzNjLWFjNmYtMDk3N2FmM2JiM2M1XkEyXkFqcGdeQXVyNjY5NDU4NzI@._V1_SX300.jpg",
       Year: "1994",
     },
     {
       Title: "The Lion King 2: Simba's Pride",
       imdbID: "tt0110358",
       Type: "movie",
-      Poster: "https://imageofthelionking2.jpg",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BY2Y3MTk2MDgtOTc1Yy00ZmFjLThlNTEtMDQ1Y2EzZmRjMzVjXkEyXkFqcGdeQXVyNjk1Njg5NTA@._V1_SX300.jpg",
       Year: "1998",
     },
     {
       Title: "The Lion King 3: Hakuna Matata",
       imdbID: "tt0110359",
       Type: "movie",
-      Poster: "https://imageofthelionking3.jpg",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BOGZiMDQ1YTQtMGVlOC00OTJiLWFkYzMtYjdiYmY0NWM5YzVjXkEyXkFqcGdeQXVyOTg4MDk3MTQ@._V1_SX300.jpg",
       Year: "2004",
     },
   ],
--- cypress/e2e/movie.cy.ts
+++ cypress/e2e/movie.cy.ts
@@ -2,58 +2,57 @@ import { groupBy, isObjectLike } from "cypress/types/lodash";
 import { mockData } from "../e2e/__mocks__/movieservice";
 import { noMockData } from "../e2e/__mocks__/movieservice";
 
-describe("should test movieApp successfully", () => {
-  it("should visit website/localhost", () => {
-    cy.visit("http://localhost:1234");
-  });
+beforeEach(() => {
+  cy.visit("/");
+});
 
+describe("should test movieApp successfully", () => {
   it("should have a textbox/input-field", () => {
-    cy.visit("http://localhost:1234");
     cy.get("input#searchText");
   });
 
   it("should be able to write something in textbox", () => {
-    cy.visit("http://localhost:1234");
     cy.get("input#searchText").type("Batman").should("have.value", "Batman");
   });
 
   it("should have one button", () => {
-    cy.visit("http://localhost:1234");
     cy.get("button#search").contains("Sök");
   });
 
   it("should be able to press button", () => {
-    cy.visit("http://localhost:1234");
     cy.get("button#search").contains("Sök");
     cy.get("button#search").click();
   });
 
-  it("should get mocked omdb-api", () => {
-    cy.visit("http://localhost:1234");
-    cy.intercept("GET", "http://omdbapi.com/*");
+  it("should get actual data by calling actual API", () => {
+    cy.get("input#searchText").type("The Lion King");
+    cy.get("input#searchText").should("have.value", "The Lion King");
+    cy.get("button#search").click();
+
+    cy.get("h3:first").contains("The Lion King");
   });
 
-  it("should check that mockData contains The Lion King", () => {
-    cy.visit("http://localhost:1234");
+  it("should get mockData", () => {
     cy.intercept("GET", "http://omdbapi.com/*", mockData).as("movielist");
     cy.get("input#searchText").type("The Lion King");
     cy.get("input#searchText").should("have.value", "The Lion King");
     cy.get("button#search").click();
 
     cy.wait("@movielist").its("request.url").should("contain", "Lion");
+    cy.get("h3:first").contains("The Lion King");
   });
 
-  it("should check that mockData contains 4 movies", () => {
-    cy.visit("http://localhost:1234");
+  it("should check that mockData create 4 div-elements with movies", () => {
     cy.intercept("GET", "http://omdbapi.com/*", mockData).as("movielist");
 
-    cy.get("form#searchForm").submit();
+    cy.get("input#searchText").type("The Lion King");
+    cy.get("input#searchText").should("have.value", "The Lion King");
+    cy.get("button#search").click();
 
-    cy.get("div#movie-container > div.movies").should("have.length", 0);
+    cy.get("div.movie").should("have.length", 4);
   });
 
-  it("should check that mockData have h3-headings", () => {
-    cy.visit("http://localhost:1234");
+  it("should check that mockData create element to show h3-headings", () => {
     cy.intercept("GET", "http://omdbapi.com/*", mockData).as("movielist");
     cy.get("input#searchText").type("The Lion King");
     cy.get("input#searchText").should("have.value", "The Lion King");
@@ -64,8 +63,7 @@ describe("should test movieApp successfully", () => {
     cy.get("h3").should("have.length", 4);
   });
 
-  it("should check that mockData have images", () => {
-    cy.visit("http://localhost:1234");
+  it("should check that mockData create element to show images", () => {
     cy.intercept("GET", "http://omdbapi.com/*", mockData).as("movielist");
     cy.get("input#searchText").type("The Lion King");
     cy.get("input#searchText").should("have.value", "The Lion King");
@@ -76,8 +74,7 @@ describe("should test movieApp successfully", () => {
     cy.get("div.movie > img").should("have.length", 4);
   });
 
-  it("should create all html for mockData", () => {
-    cy.visit("http://localhost:1234");
+  it("should create all html with mockData", () => {
     cy.intercept("GET", "http://omdbapi.com/*", mockData).as("movielist");
     cy.get("input#searchText").type("The Lion King");
     cy.get("input#searchText").should("have.value", "The Lion King");
@@ -89,8 +86,17 @@ describe("should test movieApp successfully", () => {
     cy.get("div.movie > img").should("have.length", 4);
   });
 
+  it("should create all html with actual data", () => {
+    cy.get("input#searchText").type("The Lion King");
+    cy.get("input#searchText").should("have.value", "The Lion King");
+    cy.get("button#search").click();
+
+    cy.get("div > div.movie").should("have.length", 10);
+    cy.get("div.movie > h3").should("have.length", 10);
+    cy.get("div.movie > img").should("have.length", 10);
+  });
+
   it("should get last heading", () => {
-    cy.visit("http://localhost:1234");
     cy.intercept("GET", "http://omdbapi.com/*", mockData).as("movielist");
     cy.get("input#searchText").type("The Lion King");
     cy.get("input#searchText").should("have.value", "The Lion King");
@@ -101,43 +107,41 @@ describe("should test movieApp successfully", () => {
     cy.get("div.movie:last > h3").contains("The Lion King 3: Hakuna Matata");
   });
 
-  it("should get second image", () => {
-    cy.visit("http://localhost:1234");
+  it("should get first image", () => {
     cy.intercept("GET", "http://omdbapi.com/*", mockData).as("movielist");
     cy.get("input").type("The Lion King");
     cy.get("input").should("have.value", "The Lion King");
     cy.get("button").click();
 
-    cy.wait("@movielist").its("request.url").should("contain", "Lion");
-
-    cy.get("div.movie > img:nth-child(2)").contains(
-      "https://imageofthelionking.jpg"
+    cy.get("img").should(
+      "have.attr",
+      "src",
+      "https://m.media-amazon.com/images/M/MV5BYTYxNGMyZTYtMjE3MS00MzNjLWFjNmYtMDk3N2FmM2JiM2M1XkEyXkFqcGdeQXVyNjY5NDU4NzI@._V1_SX300.jpg"
     );
   });
-});
 
-describe("should test movieApp unsuccessfully", () => {
+  it("all div should have class movie", () => {
+    cy.intercept("GET", "http://omdbapi.com/*", mockData).as("movielist");
+    cy.get("input").type("The Lion King");
+    cy.get("input").should("have.value", "The Lion King");
+    cy.get("button").click();
+
+    cy.get("div").should("have.class", "movie");
+  });
+
   it("should get error message if no input", () => {
-    cy.visit("http://localhost:1234");
     cy.get("button#search").click();
     cy.get("input#searchText").should("have.value", "");
     cy.get("p").should("contain", "Inga sökresultat att visa");
   });
 
   it("should check that noMockData contains 0 movies", () => {
-    cy.visit("http://localhost:1234");
     cy.intercept("GET", "http://omdbapi.com/*", noMockData).as("nomovielist");
 
-    cy.get("form#searchForm").submit();
+    cy.get("input#searchText").type("Film som inte finns");
+    cy.get("input").should("have.value", "Film som inte finns");
+    cy.get("button").click();
 
     cy.get("div#movie-container > div.movies").should("have.length", 0);
   });
 });
-
-// describe("control localStorage", () => {
-//   it("should find something in localStorage", () => {
-//     localStorage.setItem("movies", JSON.stringify(mockedMovies));
-//     cy.visit("http://localhost:1234");
-//     cy.get("div > li").should("have.length", 4);
-//   });
-// });
